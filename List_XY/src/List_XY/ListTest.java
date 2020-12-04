afa14674f99e727fcb21c92b7310863920ffc565
Robi <jozsar82@gmail.com>
2020-12-04 13:33:27 +0100

    SWENG

---
+++ List_XY/src/List_XY/ListTest.java
@@ -0,0 +1,45 @@
+package List_XY;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+
+public class ListTest {
+
+	private List people= new List();
+	@Before
+	public void init() {
+		this.people.add("Andor");
+		this.people.add("Elza");
+		this.people.add("Vivien");
+		this.people.add("Xavér");
+		this.people.add("Barbara");
+	}
+	@Test
+	public void testSize() {
+		assertEquals("Méret Ellenõrzés", 5, this.people.size());
+		}
+	@Test
+	public void testEmpty() {
+		assertFalse(this.people.isEmpty());
+	}
+	@Test
+	public void testAdd() {
+		this.people.add("Vilma");
+		assertEquals("Új név hozzáadásának ellenõrzése",6,this.people.size());
+	}
+	@Test
+	public void testremoveAll() {
+		this.people.removeAll();
+		assertTrue(this.people.isEmpty());
+		}
+	@After
+	public void destroy(){
+		this.people.removeAll();
+	}
+}
